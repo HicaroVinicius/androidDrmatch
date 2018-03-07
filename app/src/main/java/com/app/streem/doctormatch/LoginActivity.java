c471cc2626f581fb51c16c8f2f238bba28ed764c
Josué Azevedo <josue-azevedo@hotmail.com>
2018-03-07 07:42:44 -0300

    Revert "Revert "Activity Datail""
    This reverts commit b6aa47973318c36272ed4188e8286b43e99a0944.

--- app/src/main/java/com/app/streem/doctormatch/LoginActivity.java
+++ app/src/main/java/com/app/streem/doctormatch/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -15,6 +16,12 @@ import android.widget.Toast;
 
 import com.app.streem.doctormatch.DAO.Firebase;
 import com.app.streem.doctormatch.DAO.Preferencias;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -25,6 +32,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -91,6 +99,7 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 }
             });
+
         }else{
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
